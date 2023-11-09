45bd227228b51e6bdf91415aec5897bc92b9a071
Rohan2617 <90930741+Rohan2617@users.noreply.github.com>
2023-11-09 23:22:31 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@
  * add event on element
  */
 
+
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
@@ -84,3 +85,144 @@ const filter = function () {
 }
 
 addEventOnElem(filterBtns, "click", filter);
+
+
+/**
+ * 
+ * Currency Converter
+ */
+
+let array1=[["$",1],["Eu.",2],["Rs.",83]]
+const dPricesObj=document.getElementsByClassName("price");
+const dPrices=[];
+for (var i=0;i<dPricesObj.length;i++)
+{
+  dPrices.push(dPricesObj[i].innerHTML);
+}
+for (var i=0;i<dPrices.length;i++)
+{
+  
+  dPrices[i]=(parseFloat(dPrices[i].slice(1)));
+  console.log(dPrices[i]);
+}
+function priceChange(factor,currency)
+{
+  const prices=document.getElementsByClassName("price");
+  for (var i=0;i<dPrices.length;i++)
+  {
+    let inHtml=Math.round(parseFloat(dPrices[i])*factor).toFixed(2);
+    prices[i].innerHTML=currency+inHtml;
+  }
+}
+const C=document.getElementsByClassName("currency");
+for (var i=0;i<C.length;i++)
+{
+  console.log(C[i]);
+  let currency=array1[i][0];
+  let factor=array1[i][1];
+  C[i].addEventListener("click",function()
+{
+  document.getElementsByClassName("currentC")[0].classList.remove("currentC");
+  this.classList.add("currentC");
+priceChange(factor,currency)
+})
+}
+
+
+var n= document.getElementsByClassName("card-action-btn").length;
+
+console.log(n);
+for (var i=2;i<n;i+=3)
+{
+  var wishlistBtn=document.getElementsByClassName("card-action-btn")[i];
+  var wishlist_count=document.getElementsByClassName("btn-badge")[0];
+  wishlistBtn.addEventListener("click",function()
+  {
+    let icon=this.children[0];
+    //icon.href="javascript:void()";
+    if ((icon.name).localeCompare("heart"))
+    {
+      wishlist_count.innerHTML=parseInt(wishlist_count.innerHTML)+1;
+      this.children[0].name="heart";
+    }
+    else{
+      wishlist_count.innerHTML=parseInt(wishlist_count.innerHTML)-1;
+      this.children[0].name="heart-outline";
+    }
+  })
+}
+
+for (var i=1;i<n;i+=3)
+{
+  var cartBtn=document.getElementsByClassName("card-action-btn")[i];
+  var bag_count=document.getElementsByClassName("btn-badge")[1];
+  cartBtn.addEventListener("click",function()
+  {
+    let icon=this.children[0];
+    let ele = this.parentElement.parentElement.parentElement.parentElement;
+    console.log(ele.children[1].children[1].children[0].innerHTML);
+    var src=this.parentElement.parentElement.parentElement.children[0].src;
+    var name=ele.children[1].children[0].children[0].innerHTML;
+    var price=ele.children[1].children[1].children[0].innerHTML;
+    addItemToCart(src,name,price)
+    if ((icon.name).localeCompare("bag-handle"))
+    {
+      bag_count.innerHTML=parseInt(bag_count.innerHTML)+1;
+      this.children[0].name="bag-handle";
+    }
+    else{
+      bag_count.innerHTML=parseInt(bag_count.innerHTML)-1;
+      this.children[0].name="bag-handle-outline";
+    }
+  })
+}
+
+
+
+var cartBtn=document.getElementsByClassName("header-action-btn")[2];
+console.log(cartBtn)
+cartBtn.addEventListener("click",function()
+{
+  var cartArea=document.getElementsByClassName("cartArea")[0].style.display="block";
+  
+});
+var click=0;
+if (click==1)
+{
+  cartBtn.addEventListener()
+}
+cartBtn.addEventListener("dblclick",function()
+{
+  var cartArea=document.getElementsByClassName("cartArea")[0].style.display="none";
+})
+var x=0;
+var cartArea=document.getElementsByClassName("cartArea")[0];
+
+function addItemToCart(src,name,price)
+{
+  var newItem=document.createElement("div");
+    newItem.id="item"+x;
+    newItem.classList.add("items");
+  var newImg=document.createElement("img");
+    newImg.src=src;
+    newImg.classList.add("item-img");
+  var itemDetails=document.createElement("div");
+    itemDetails.classList.add("item-details");
+  var label1=document.createElement("label");
+    label1.classList.add("item-labels");
+    label1.innerHTML=name;
+  var label2=document.createElement("label");
+  label2.classList.add("item-labels");
+    label2.innerHTML=price;
+   
+  var crossButton=document.createElement("div");
+    crossButton.innerHTML="<ion-icon name='bag-handle' aria-hidden='true'></ion-icon>";
+    crossButton.classList.add("item-del-btn");
+    itemDetails.appendChild(label1);
+    itemDetails.appendChild(label2);
+    
+    newItem.appendChild(newImg);
+    newItem.appendChild(itemDetails);
+    newItem.appendChild(crossButton)
+    cartArea.appendChild(newItem);
+}
